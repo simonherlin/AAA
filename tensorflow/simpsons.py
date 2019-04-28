dc9e23719266945fdfc28f9ee229dc66ebcbbaf0
simonherlin <herlin.simon@gmail.com>
2019-04-28 21:07:15 +0200

    0.89 good in test

--- tensorflow/simpsons.py
+++ tensorflow/simpsons.py
@@ -4,6 +4,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 import keras
+import matplotlib.pyplot as plt
 
 K.tensorflow_backend._get_available_gpus()
 # dimensions of our images.
@@ -11,7 +12,7 @@ img_width, img_height = 150, 150
 
 train_data_dir = './simpsons_dataset/'
 
-epochs = 50
+epochs = 30
 batch_size = 16
 
 input_shape = (img_width, img_height, 3)
@@ -37,21 +38,37 @@ model.add(tf.keras.layers.Dense(nb_classes, activation='softmax'))
 """
 
 model = Sequential()
-model.add(keras.layers.Conv2D(200, kernel_size=kernel_size,activation='relu', input_shape=input_shape))
+
+model.add(keras.layers.Conv2D(32, kernel_size=kernel_size,activation='relu', input_shape=input_shape))
 model.add(keras.layers.MaxPooling2D(pool_size=pool_size))
-model.add(keras.layers.Dropout(0.25))
-model.add(keras.layers.Flatten())
-# model.add(keras.layers.Dropout(0.5))
-# model.add(keras.layers.Dense(256, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
+
+model.add(keras.layers.Conv2D(64, kernel_size=kernel_size,activation='relu', input_shape=input_shape))
+model.add(keras.layers.MaxPooling2D(pool_size=pool_size))
+model.add(keras.layers.Dropout(0.5))
+
+
+model.add(keras.layers.Conv2D(128, kernel_size=kernel_size,activation='relu', input_shape=input_shape))
+model.add(keras.layers.MaxPooling2D(pool_size=pool_size))
+model.add(keras.layers.Dropout(0.5))
+
+
+model.add(keras.layers.Conv2D(256, kernel_size=kernel_size,activation='relu', input_shape=input_shape))
+model.add(keras.layers.MaxPooling2D(pool_size=pool_size))
+model.add(keras.layers.Dropout(0.5))
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.5))
+#model.add(keras.layers.Dense(256, activation='relu'))
+#model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.Dense(128, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(64, activation='relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(32, activation='relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(16, activation='relu'))
-model.add(keras.layers.Dropout(0.5))
+# model.add(keras.layers.Dense(64, activation='relu'))
+# model.add(keras.layers.Dropout(0.5))
+# model.add(keras.layers.Dense(32, activation='relu'))
+# model.add(keras.layers.Dropout(0.5))
+# model.add(keras.layers.Dense(16, activation='relu'))
+# model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.Dense(8, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
 model.summary()
@@ -76,14 +93,17 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-
 validation_generator = test_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='categorical')
 
-model.fit_generator(
+
+STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID=validation_generator.n//validation_generator.batch_size
+
+history = model.fit_generator(
     train_generator,
     steps_per_epoch=nb_img // batch_size,
     epochs=epochs,
@@ -91,9 +111,43 @@ model.fit_generator(
     validation_steps= nb_img // batch_size)
 
 
+# loss_curve = history.history["loss"]
+# acc_curve = history.history["accuracy"]
+#
+# loss_val_curve = history.history["val_loss"]
+# acc_val_curve = history.history["val_accuracy"]
+#
+# plt.plot(loss_curve, label="Train")
+# plt.plot(loss_val_curve, label="Val")
+# plt.legend(loc='upper left')
+# plt.title("Loss")
+# plt.show()
+#
+# plt.plot(acc_curve, label="Train")
+# plt.plot(acc_val_curve, label="Val")
+# plt.legend(loc='upper left')
+# plt.title("Accuracy")
+# plt.show()
 
-score = model.evaluate(x_test, y_test, verbose=0)
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+score = model.evaluate_generator(generator=validation_generator,
+                                 steps=STEP_SIZE_VALID)
 print('Test loss: ', score[0])
 print('Test accuracy: ', score[1])
-
 
